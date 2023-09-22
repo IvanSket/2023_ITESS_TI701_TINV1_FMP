1df61d03aaca2d867c3cadeb917150e8f066a4fa
IvanSket <dipp3er123@gmail.com>
2023-09-22 16:29:00 -0500

    First commit

---
+++ src/pkg2023_itess_tics701/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg2023_itess_tics701;
+
+/** Itess-Tics
+ *Taller de investigaccion 1
+ * @author Ivan
+ /** Intento de curso de certificado
+ * 22/09/23
+ */
+public class Main {
+
+    public static void main(String...arg) {
+       
+    }
+    
+}
